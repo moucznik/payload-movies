ceaee0f673afd3d831129dfc790ff7fd7b87bbdc
Jiri Mouka <jiri.mouka@notum.cz>
2024-11-01 13:08:22 +0100

    migrations removed

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241025_123433_initial from './20241025_123433_initial';
-
-export const migrations = [
-  {
-    up: migration_20241025_123433_initial.up,
-    down: migration_20241025_123433_initial.down,
-    name: '20241025_123433_initial'
-  },
-];
